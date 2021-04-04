21134a38baf2fa0837a6bff9baff8e842c53de86
Arvind nishad <78343586+arvind-timbr@users.noreply.github.com>
2021-04-04 11:58:24 +0530

    huskey in action

--- src/App.js
+++ src/App.js
@@ -2,11 +2,39 @@ import React from 'react'
 
 // Remove the working code and Uncomment below to see huskey work in action. Please run 'git add . && git commit -m "huskey in action"'
 
+/**
+ * No space example
+ */
+
 // function App() {return <div className='App'>Hello World</div>;}
 // export default App;
 
+/**
+ *
+ * Too much space example
+ */
+// function App() {
+//     return <div className="App">Hello World</div>
+// }
+
+// export default App
+
+/**
+ * No tab space example
+ */
+
 function App() {
-    return <div className="App">Hello World</div>
+    return (
+        <div className="App">
+            <div>Hello World</div>
+        </div>
+    )
 }
 
 export default App
+
+// function App() {
+//     return <div className="App">Hello World</div>
+// }
+
+// export default App
