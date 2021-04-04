62897b329425bab007027b61b97dcf12e5f96582
Arvind nishad <78343586+arvind-timbr@users.noreply.github.com>
2021-04-04 11:49:09 +0530

    huskey in action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+// Uncomment below to see huskey work in action
+
+// function App() {return <div className='App'>Hello World</div>;}
+// export default App;
+
 function App() {
     return <div className="App">Hello World</div>
 }
