1b6dbbc06b12fdb7709202a6f0e4aaefff4959ff
Arvind nishad <78343586+arvind-timbr@users.noreply.github.com>
2021-04-04 11:54:12 +0530

    huskey in action

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-// Uncomment below to see huskey work in action
+// Remove the working code and Uncomment below to see huskey work in action. Please run 'git add . && git commit -m "huskey in action"'
 
 // function App() {return <div className='App'>Hello World</div>;}
 // export default App;
